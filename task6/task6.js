52adbe19936f33ab70642a559b723486c271a57b
sogabeharuka <135292763+sogabeharuka@users.noreply.github.com>
2023-06-23 09:46:45 +0900

    task6 fix

--- task6/task6.js
+++ task6/task6.js
@@ -2,13 +2,11 @@
     'use strict';
   
     kintone.events.on('app.record.create.show', (event) => {
-      console.log(event);
       const action5Array = ['あくなき探求', '不屈の心体', '理想への共感', '心を動かす', '知識を増やす', '公明正大'];
       const record = event.record;
       const subtableArray = record.Table.value;
       subtableArray.shift();
   
-      console.log(subtableArray);
       action5Array.forEach((element) => {
         subtableArray.push({
           value: {
