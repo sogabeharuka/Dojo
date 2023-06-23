25224c3d6158dceb8512ac5e1941936f803f3af9
sogabeharuka <135292763+sogabeharuka@users.noreply.github.com>
2023-06-23 14:01:10 +0900

    fix task7

--- task7/task7.js
+++ task7/task7.js
@@ -1,32 +1,32 @@
 (() => {
-    'use strict';
-    const eventTools = [
-      'app.record.create.show',
-      'app.record.create.change.日付',
-      'app.record.create.change.サイボウズ製品',
-      'app.record.create.change.管理番号',
-      'app.record.edit.show',
-      'app.record.edit.change.日付',
-      'app.record.edit.change.サイボウズ製品',
-      'app.record.edit.change.管理番号'];
-  
-    kintone.events.on(eventTools, (event) => {
-      const dataFormat = dateFns.format(event.record.日付.value, 'YYYYMMDD');
-      const product = event.record.サイボウズ製品.value;
-      const identifyNum = event.record.管理番号.value ? event.record.管理番号.value:'';
-      let productId = '';
-  
-      if (product === 'kintone') {
-        productId = 'KN';
-      } else if (product === 'Garoon') {
-        productId = 'GR';
-      } else if (product === 'サイボウズ Office') {
-        productId = 'OF';
-      } else if (product === 'Mailwise') {
-        productId = 'MW';
+  'use strict';
+  const eventTools = [
+    'app.record.create.show',
+    'app.record.create.change.日付',
+    'app.record.create.change.サイボウズ製品',
+    'app.record.create.change.管理番号',
+    'app.record.edit.show',
+    'app.record.edit.change.日付',
+    'app.record.edit.change.サイボウズ製品',
+    'app.record.edit.change.管理番号'];
+
+  kintone.events.on(eventTools, (event) => {
+    const getSelectedProductName = (productValue) => {
+      switch (productValue) {
+        case 'kintone':
+          return 'KN';
+        case 'Garoon':
+          return 'GR';
+        case 'サイボウズ Office':
+          return 'OF';
+        case 'Mailwise':
+          return 'MW';
       }
-  
-      event.record.重複禁止項目_文字列.value = `${dataFormat}-${productId}-${identifyNum}`;
-      return event;
-    });
-  })();
+    };
+    const dataFormat = dateFns.format(event.record.日付.value, 'YYYYMMDD');
+    const identifyNum = event.record.管理番号.value ? event.record.管理番号.value:'';
+    const productId = getSelectedProductName(event.record.サイボウズ製品.value);
+    event.record.重複禁止項目_文字列.value = `${dataFormat}-${productId}-${identifyNum}`;
+    return event;
+  });
+})();
