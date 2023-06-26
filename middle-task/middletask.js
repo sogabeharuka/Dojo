1f648cf55c22c473133b5b0087e66c5976666c39
sogabeharuka <135292763+sogabeharuka@users.noreply.github.com>
2023-06-26 18:52:58 +0900

    middle-task

--- middle-task/middletask.js
+++ middle-task/middletask.js
@@ -1,5 +1,6 @@
-(() =>{
+(async() => {
     'use strict';
+    const tableElement = document.getElementById("table-news-body");
 
     const params = 'dojo';
     const url=`https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo?id=${params}`;
@@ -9,11 +10,31 @@
         console.log(jsonData);
         return jsonData;
     }
-    const data= fetchAdress();
-
-   const tr = document.createElement('tr');
-   const datetd = document.createElement('td');
-   const categorytd = document.createElement('td');
-   const 
+    const createTable = (tableData) => {
+        tableData.forEach(element => {
+            
+            const rowCownt = tableElement.rows.length;
+            const firstRow = tableElement.insertRow(rowCownt);
+            
+            const dateCell = firstRow.insertCell(0);
+            const categoryCell = firstRow.insertCell(1);
+            const titledCell = firstRow.insertCell(2);
+            console.log(tableData);
+            
+            dateCell.textContent = element.day.value;
+            categoryCell.textContent = element.category.value;
+            console.log(element);
+            
+            const titled = document.createElement('a');
+            const createText = document. createTextNode(element.content.value);
+            titled.setAttribute('href',element.url.value);
+            titled.setAttribute('target',element.target.value);
+            titled.appendChild(createText);
+            titledCell.appendChild(titled);
+            
+        });
+    }
+    const data = await fetchAdress();
+    createTable(data);
 
 })();
