e8e0444a02d8f486d4034aad0223e9e7703cb26c
sogabeharuka <135292763+sogabeharuka@users.noreply.github.com>
2023-06-27 17:49:31 +0900

    middle-task

--- middle-task/middletask.js
+++ middle-task/middletask.js
@@ -1,40 +1,48 @@
-(async() => {
+(async () => {
     'use strict';
-    const tableElement = document.getElementById("table-news-body");
-
+    const tableElement = document.getElementById('table-news-body');
+  
     const params = 'dojo';
-    const url=`https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo?id=${params}`;
-    const fetchAdress = async() => {
-        const apiUrl = await fetch(url); 
-        const jsonData = await apiUrl.json();
-        console.log(jsonData);
-        return jsonData;
-    }
+    const url =`https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo?id=${params}`;
+    const fetchAdress = async () => {
+      const apiUrl = await fetch(url);
+      const jsonData = await apiUrl.json();
+      return jsonData;
+    };
+  
+    const addCategoryBackgroundColor = (categoryType) => {
+      if (categoryType === '企業情報') {
+        return '#87cefa';
+      } else if (categoryType === '製品') {
+        return '#7fffd4';
+      } else {
+        return '#ff6347';
+      }
+    };
+  
     const createTable = (tableData) => {
-        tableData.forEach(element => {
-            
-            const rowCownt = tableElement.rows.length;
-            const firstRow = tableElement.insertRow(rowCownt);
-            
-            const dateCell = firstRow.insertCell(0);
-            const categoryCell = firstRow.insertCell(1);
-            const titledCell = firstRow.insertCell(2);
-            console.log(tableData);
-            
-            dateCell.textContent = element.day.value;
-            categoryCell.textContent = element.category.value;
-            console.log(element);
-            
-            const titled = document.createElement('a');
-            const createText = document. createTextNode(element.content.value);
-            titled.setAttribute('href',element.url.value);
-            titled.setAttribute('target',element.target.value);
-            titled.appendChild(createText);
-            titledCell.appendChild(titled);
-            
-        });
-    }
+      tableData.forEach(element => {
+        const rowCownt = tableElement.rows.length;
+        const firstRow = tableElement.insertRow(rowCownt);
+  
+        const dateCell = firstRow.insertCell(0);
+        const categoryCell = firstRow.insertCell(1);
+        const titledCell = firstRow.insertCell(2);
+        dateCell.textContent = element.day.value;
+  
+        let categoryType = element.category.value;
+        categoryCell.textContent = categoryType;
+  
+        categoryCell.style.backgroundColor = addCategoryBackgroundColor(categoryType);
+  
+        const titled = document.createElement('a');
+        const createText = document.createTextNode(element.content.value);
+        titled.setAttribute('href', element.url.value);
+        titled.setAttribute('target', element.target.value);
+        titled.appendChild(createText);
+        titledCell.appendChild(titled);
+      });
+    };
     const data = await fetchAdress();
     createTable(data);
-
-})();
+  })();
