6c8a1e8accce068682f9b38b9e7be71c58cdef3e
sogabeharuka <135292763+sogabeharuka@users.noreply.github.com>
2023-06-28 17:45:32 +0900

    middle-task

--- middle-task/middletask.js
+++ middle-task/middletask.js
@@ -6,39 +6,38 @@
     const url =`https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo?id=${params}`;
     const fetchAdress = async () => {
       const apiUrl = await fetch(url);
-      const jsonData = await apiUrl.json();
+      const jsonData = apiUrl.json();
       return jsonData;
     };
-    
+  
     const addCategoryBackgroundColor = (categoryType) => {
-        if (categoryType === '企業情報') {
-            return '#87cefa';
-        } else if (categoryType === '製品') {
-            return '#7fffd4';
-        } else {
-            return '#ff6347';
-        }
+      if (categoryType === '企業情報') {
+        return '#87cefa';
+      } else if (categoryType === '製品') {
+        return '#7fffd4';
+      } else {
+        return '#ff6347';
+      }
     };
+  
     const createTable = (tableData) => {
-        tableData.forEach(element => {
-            const rowCownt = tableElement.rows.length;
-            const firstRow = tableElement.insertRow(rowCownt);
-            console.log(tableData);
+      tableData.forEach(element => {
+        const firstRow = tableElement.insertRow(tableElement.rows.length);
   
         const dateCell = firstRow.insertCell(0);
         const categoryCell = firstRow.insertCell(1);
         const titledCell = firstRow.insertCell(2);
         dateCell.textContent = element.day.value;
   
-        let categoryType = element.category.value;
+        const categoryType = element.category.value;
         categoryCell.textContent = categoryType;
   
         categoryCell.style.backgroundColor = addCategoryBackgroundColor(categoryType);
   
         const titled = document.createElement('a');
         const createText = document.createTextNode(element.content.value);
-        titled.setAttribute('href', element.url.value);
-        titled.setAttribute('target', element.target.value);
+        titled.href = element.url.value;
+        titled.target = element.target.value;
         titled.appendChild(createText);
         titledCell.appendChild(titled);
       });
